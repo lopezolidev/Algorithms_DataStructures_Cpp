abdb390b056132a156f498b119422948c8938c35
rockwatch <olivera9806@gmail.com>
2023-12-06 12:32:45 -0400

    [ADD] quicksort algorithm

---
+++ practice_02/02_03.cpp
@@ -0,0 +1,19 @@
+/*
+A shepherd stands on a river bank with a sheep, a wolf and a cabbage. To cross to the other shore he has a very small boat in which he can only cross accompanied by at most one of the three. Make a algorithm (based on the Backtracking technique) that finds a way for the shepherd to move the sheep, to the wolf and the cabbage, knowing that if at any given moment he leaves the wolf and the sheep alone, it will eat it; and of the same. So, if he left them alone, the sheep would eat the cabbage.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void boat(char animals[], int n){
+
+}
+
+int main(){
+    char p[] = {'K', 'c', 'L'};
+
+    boat(p, 3);
+
+    return 0;
+}
