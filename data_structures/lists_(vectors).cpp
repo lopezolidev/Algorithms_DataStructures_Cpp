8620345847cd779ed39507f6e2d9d4bfc28f5611
rockwatch <olivera9806@gmail.com>
2024-02-03 21:18:48 -0400

    [PUT] modifying methods of array list. Simplifying insertion and deletion of elements calling insertion with specified parameters. Extraction of first and last elements of the list

--- data_structures/lists_(vectors).cpp
+++ data_structures/lists_(vectors).cpp
@@ -32,9 +32,11 @@ class List {
 
         bool insert(int e, int pos);
         bool deleter(int pos);
-        void push_start(int e);
-        void push_end(int e);
+        bool push_start(int e);
+        bool push_end(int e);
         int* get_pos(int pos); // function that returns the memory address of this element in that position
+        int pop();
+        int shift();
 
         int get_capacity(){
             return this->capacity; 
@@ -55,7 +57,7 @@ To-be implemented:
 bool List::filler(){
     int i = 0;
     while(i < capacity){
-        this->elements[i] = 0;
+        this->elements[i] = -1;
         i++;
     }
 
@@ -81,7 +83,7 @@ bool List::insert(int e, int pos){
     }   //if the position to insert the element is greater or less than the dimensions of the array
     
     if(this->size + 1 > this->capacity){
-        resize(this->capacity + 1);
+        resize(this->capacity + this->step);
     } //resizing the array when the insertion of the element exceeds the dimensions of the array
 
     int i = size - 1;
@@ -101,46 +103,50 @@ bool List::deleter(int pos){
     } //if the position to delete the element is greater or less than the dimensions of the array
 
     int i = pos + 1;
-    while(i < size){
+    while(i < this->size){
         this->elements[i - 1] = this->elements[i];
         i++;
     } //sweeping elements backwards one position to delete the desired element
 
-    this->elements[size] = NULL;        // Why error????
-    this->size--;
+    this->elements[i] = -1;        
+    this->size--; //reducing the size before contraction of the vector
+
+    if(this->elements[size] == -1){
+        resize(i+1);
+    }   //contracting the vector after deletion with available elements + 1
+    
     return true; 
 } //deleting elements in a desired position
 
 int* List::get_pos(int pos){
-    if(pos >= this->size){
+    if(pos < 0 || pos >= this->size){
         return NULL;
     }
     return &this->elements[pos];
 }   // if the position of that element is not in the array return NULL
 
-void List::push_start(int e){
-    if(this->size + 1 > this->capacity){
-        this->resize(this->capacity + this->step);
-    }
-    
-    int i = size - 1; //iterator in last position
-    while(i >= 0){
-        this->elements[i+1] = this->elements[i];
-        i--;
-    }   //sweeping elements forward one position to insert them in the desired position
+bool List::push_start(int e){
+    return insert(e , 0); 
+}   //inserts the element at the start of the list
 
-    this->elements[0] = e; //element now in the first position
-    this->size++;
-}   //the element is at the start of the list
+bool List::push_end(int e){
+    return insert(e, this->size -1);
+} //inserts the element at the end of the list
+   
+int List::pop(){
+    int v = this->elements[0];
+    deleter(0);
+    return v;
+} //deletes the element at the start of the list
 
-void List::push_end(int e){
-    if(this->size + 1 > this->capacity){
-        this->resize(this->capacity + this->step); //enlarging the array
-    }
-    this->elements[this->size] = e; //inserting the element at the end of the array
-    size++; //increasing the size (number of elements) in the list
-}
+int List::shift(){
+    int v = this->elements[this->size - 1];
+    deleter(this->size - 1);
+    return v;
+} //deletes the element at the end of the list
 
+
+// Further testing must be executed
 
 int main(){
 
