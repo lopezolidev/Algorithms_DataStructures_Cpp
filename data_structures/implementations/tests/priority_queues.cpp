f916bd99806b9809398055086ef2bbececcd76d1
rockwatch <olivera9806@gmail.com>
2024-02-09 12:00:00 -0400

    [ADD] priority queues algorithm

--- data_structures/implementations/tests/priority_queues.cpp
+++ data_structures/implementations/tests/priority_queues.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
-#include <stdio>
+#include <queue>
 
 using namespace std;
 
-class
+template <class T>
+class MyQueue{
+    private:
+        queue<T>* priorityQueues;
+        int _size;
+    public: 
+        MyQueue(int n){
+            priorityQueues = new queue<T>[n];
+            this->_size = 0;
+        }
+        ~MyQueue(){
+            delete[] priorityQueues;
+        }
+        void enqueue(T z, int prio){
+            if(prio < 0 || prio > n - 1){
+                cout << "invalid " << endl; return ;
+            }
+            priorityQueues[prio].push(z);
+        }
+        T* dequeue(){
+            int i = 0;
+            while(i < n - 1){
+                if(!priorityQueues[i].isEmpty()){
+                    T* v = priorityQueues[i].pop();
+                    return v;
+                }
+                i++;
+            }
+        }
 
+};
 
 int main(){
-
+    return 0;
 }
